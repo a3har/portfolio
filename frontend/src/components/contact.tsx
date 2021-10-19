10c98a121464621ed03d77c2041e82659fb167b8
Muhammad Azhar <azharmuham13@hotmail.com>
2021-10-19 17:24:44 +0530

    uses footer from lekoarts

--- frontend/src/components/contact.tsx
+++ frontend/src/components/contact.tsx
@@ -5,14 +5,27 @@ import Inner from "../elements/inner"
 import Content from "../elements/content"
 import Svg from "./svg"
 import { UpDown, UpDownWide, waveAnimation } from "../styles/animations"
-import Footer from "./footer"
+import Footer from "@lekoarts/gatsby-theme-cara/src/components/footer"
 // @ts-ignore
 import ContactMDX from "../sections/contact"
 
-const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) => (
+const Contact = ({
+  offset,
+  factor = 1,
+}: {
+  offset: number
+  factor?: number
+}) => (
   <div>
     <Divider fill="divider" speed={0.2} offset={offset} factor={factor}>
-      <div sx={{ position: `absolute`, bottom: 0, width: `full`, transform: `matrix(1, 0, 0, -1, 0, 0)` }}>
+      <div
+        sx={{
+          position: `absolute`,
+          bottom: 0,
+          width: `full`,
+          transform: `matrix(1, 0, 0, -1, 0, 0)`,
+        }}
+      >
         <div
           sx={{
             position: `relative`,
@@ -21,7 +34,12 @@ const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) =>
             path: { animation: waveAnimation(`20s`) },
           }}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" id="contact-wave" viewBox="0 0 800 338.05" preserveAspectRatio="none">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="contact-wave"
+            viewBox="0 0 800 338.05"
+            preserveAspectRatio="none"
+          >
             <path>
               <animate
                 attributeName="d"
@@ -42,18 +60,59 @@ const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) =>
     </Content>
     <Divider speed={0.1} offset={offset} factor={factor}>
       <UpDown>
-        <Svg icon="upDown" hiddenMobile width={8} color="icon_darkest" left="70%" top="20%" />
-        <Svg icon="triangle" width={8} stroke color="icon_darkest" left="25%" top="5%" />
+        <Svg
+          icon="upDown"
+          hiddenMobile
+          width={8}
+          color="icon_darkest"
+          left="70%"
+          top="20%"
+        />
+        <Svg
+          icon="triangle"
+          width={8}
+          stroke
+          color="icon_darkest"
+          left="25%"
+          top="5%"
+        />
       </UpDown>
       <UpDownWide>
-        <Svg icon="triangle" width={12} stroke color="icon_brightest" left="95%" top="50%" />
-        <Svg icon="circle" width={6} color="icon_brightest" left="85%" top="15%" />
-        <Svg icon="upDown" hiddenMobile width={8} color="icon_darkest" left="45%" top="10%" />
+        <Svg
+          icon="triangle"
+          width={12}
+          stroke
+          color="icon_brightest"
+          left="95%"
+          top="50%"
+        />
+        <Svg
+          icon="circle"
+          width={6}
+          color="icon_brightest"
+          left="85%"
+          top="15%"
+        />
+        <Svg
+          icon="upDown"
+          hiddenMobile
+          width={8}
+          color="icon_darkest"
+          left="45%"
+          top="10%"
+        />
       </UpDownWide>
       <Svg icon="circle" width={6} color="icon_brightest" left="4%" top="20%" />
       <Svg icon="circle" width={12} color="icon_darkest" left="70%" top="60%" />
       <Svg icon="box" width={12} color="icon_darkest" left="20%" top="30%" />
-      <Svg icon="hexa" width={8} stroke color="icon_darkest" left="80%" top="70%" />
+      <Svg
+        icon="hexa"
+        width={8}
+        stroke
+        color="icon_darkest"
+        left="80%"
+        top="70%"
+      />
     </Divider>
   </div>
 )
