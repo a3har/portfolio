22af2b41ca1b3a9e5bd5bcf637233877f93e5161
Muhammad Azhar <azharmuham13@hotmail.com>
2021-10-19 10:21:25 +0530

    added more sections to homepage

---
+++ frontend/src/components/footer.tsx
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+
+const Footer = () => {
+  const [colorMode, setColorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  const toggleColorMode = (e: any) => {
+    setColorMode(isDark ? `light` : `dark`)
+  }
+
+  return (
+    <Box as="footer" variant="footer">
+      <button
+        sx={{
+          variant: `buttons.toggle`,
+          fontWeight: `semibold`,
+          display: `block`,
+          mx: `auto`,
+          mb: 3,
+        }}
+        onClick={toggleColorMode}
+        type="button"
+        aria-label="Toggle dark mode"
+      >
+        {isDark ? `Light` : `Dark`}
+      </button>
+      <br />
+    </Box>
+  )
+}
+
+export default Footer
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,26 +1,22 @@
 import * as React from "react"
-// import { Link } from "gatsby"
 
 import { Parallax } from "@react-spring/parallax"
-// import { Themed } from "theme-ui"
 import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout"
-// import Divider from "@lekoarts/gatsby-theme-cara/src/elements/divider"
-// import {
-//   UpDown,
-//   UpDownWide,
-// } from "@lekoarts/gatsby-theme-cara/src/styles/animations"
-// import Svg from "@lekoarts/gatsby-theme-cara/src/components/svg"
-import Seo from "@lekoarts/gatsby-theme-cara/src/components/seo"
+import Seo from "../components/seo"
 import Hero from "../components/hero"
-// import Content from "@lekoarts/gatsby-theme-cara/src/elements/content"
-// import Inner from "@lekoarts/gatsby-theme-cara/src/elements/inner"
+import Projects from "../components/projects"
+import About from "../components/about"
+import Contact from "../components/contact"
 
 const Home = ({ data, location }) => {
   return (
-    <Layout>
+    <Layout location={location}>
       <Seo title="Muhammad Azhar | Home" />
       <Parallax pages={5}>
         <Hero offset={0} factor={1} />
+        <Projects offset={1} factor={2} />
+        <About offset={3} factor={1} />
+        <Contact offset={4} factor={1} />
       </Parallax>
     </Layout>
   )
